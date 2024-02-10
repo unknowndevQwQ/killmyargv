04fc58b575fd2b38ed8cf5efb6310fa865da7fb7
poly000 <1348292515@qq.com>
2024-02-10 21:27:01 +0800

    refactor: extract `set_cmdline` so we could pass child_* manually for advanced test

--- tests/utils.rs
+++ tests/utils.rs
@@ -4,21 +4,36 @@ use base64::Engine;
 use sysinfo::{Pid, ProcessRefreshKind};
 use test_binary::build_test_binary;
 
+pub fn get_set_cmdline_path() -> Result<OsString> {
+    Ok(build_test_binary("set_cmdline_from_stdin", "testbin")?)
+}
+
 pub fn set_cmdline(
     inputs: impl IntoIterator<Item = impl AsRef<str>>,
     results: impl IntoIterator<Item = impl IntoIterator<Item = impl AsRef<str>>>,
 ) -> Result<()> {
-    let set_cmdline_path = build_test_binary("set_cmdline_from_stdin", "testbin")?;
+    let set_cmdline_path = get_set_cmdline_path()?;
     let mut set_cmdline = std::process::Command::new(set_cmdline_path)
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()?;
-    let mut child_stdin = Option::take(&mut set_cmdline.stdin).unwrap();
-    let mut child_stdout =
-        linereader::LineReader::new(Option::take(&mut set_cmdline.stdout).unwrap());
-    let engine = GeneralPurpose::new(&STANDARD, Default::default());
+    let child_stdin = Option::take(&mut set_cmdline.stdin).unwrap();
+    let child_stdout = Option::take(&mut set_cmdline.stdout).unwrap();
+    let child_pid = set_cmdline.id();
 
-    let pid = Pid::from_u32(set_cmdline.id());
+    set_cmdline_with_child(inputs, results, child_stdin, child_stdout, child_pid)
+}
+
+pub fn set_cmdline_with_child(
+    inputs: impl IntoIterator<Item = impl AsRef<str>>,
+    results: impl IntoIterator<Item = impl IntoIterator<Item = impl AsRef<str>>>,
+    mut child_stdin: ChildStdin,
+    child_stdout: ChildStdout,
+    child_pid: u32,
+) -> Result<()> {
+    let mut child_stdout = linereader::LineReader::new(child_stdout);
+    let pid = Pid::from_u32(child_pid);
+    let engine = GeneralPurpose::new(&STANDARD, Default::default());
     let mut system = sysinfo::System::new();
 
     for (case_str, results) in inputs.into_iter().zip(results) {
@@ -47,9 +62,13 @@ pub fn set_cmdline(
             .collect::<Vec<String>>();
         assert_eq!(actual_cmdline, &expected_cmdline);
     }
-
     Ok(())
 }
 
-use std::{error::Error, io::Write, process::Stdio};
+use std::{
+    error::Error,
+    ffi::OsString,
+    io::Write,
+    process::{ChildStdin, ChildStdout, Stdio},
+};
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
